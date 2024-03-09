eadcba019dcae767784463152c0392206cd41a51
Vicky Kyrmanidi <71782653+vickykyrman@users.noreply.github.com>
2024-03-09 15:34:52 +0100

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,11 +1,13 @@
-* The following jupyter notebook files create the challenge sets
-- number.ipynb (creates the challenge set for the capabilities of grammatical number, mention position, and syntactic role)
-- gender.ipynb (creates the challenge set for the capabilities of handling gender bias, and dealing with different pronoun types)
-- nonanaphoric_it.ipynb (create the challenge set for the capability of identifying cases of non-anaphoric "it")
+# A Challenge Set for Coreference Resolution
 
-* The following jupyter notebook files apply the two examined models to make predictions and evaluate those predictions with the selected tests.
-- apply_spacy.ipynb (applies and evaluates the neuralcoref model across all challenge subsets)
-- apply_bert.ipynb (applies and evaluates the SpanBERT model across all challenge subsets)
+#### The following jupyter notebook files create the challenge sets:
+1. `number.ipynb` (creates the challenge set for the capabilities of grammatical number, mention position, and syntactic role)
+2. `gender.ipynb` (creates the challenge set for the capabilities of handling gender bias, and dealing with different pronoun types)
+3. `nonanaphoric_it.ipynb` (create the challenge set for the capability of identifying cases of non-anaphoric "it")
 
-* The following jupyter notebook extracts and compares the scores from the evaluation of the two models with respect to each challenge subset.
--comparison.ipynb
+#### The following jupyter notebook files apply the two examined models to make predictions and evaluate those predictions with the selected tests:
+1. `apply_spacy.ipynb` (applies and evaluates the neuralcoref model across all challenge subsets)
+2. `apply_bert.ipynb` (applies and evaluates the SpanBERT model across all challenge subsets)
+
+#### The following jupyter notebook extracts and compares the scores from the evaluation of the two models with respect to each challenge subset:
+1. `comparison.ipynb`
